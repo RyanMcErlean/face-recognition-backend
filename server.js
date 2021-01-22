238576d7aa29ee130ce20639715603b17aebdc3b
Ryan McErlean <rmcerlean@dom01.intranet>
2021-01-22 22:12:42 +0100

    updating database

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const knex = require('knex')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-tapered-12552',
     user : 'postgres',
-    password : 'Fastrac123',
+    password : '',
     database : 'smart-brain'
   }
 });
