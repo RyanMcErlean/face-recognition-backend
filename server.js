0d42810317ea2ccd027e34a29ab06c2fd2eccb83
Ryan McErlean <rmcerlean@dom01.intranet>
2021-01-22 21:35:21 +0100

    fixed log!

--- server.js
+++ server.js
@@ -96,5 +96,5 @@ app.put('/image', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log("app is running on port 3000");
+  console.log(`app is running on port §{process.env.PORT}`);
 })
